cfbe1038c7ed11b2bfbefee4494c34d70df5b7e5
Jackelyn Velez <jlauravelez@gmail.com>
2019-10-04 19:56:32 +0200

    commencing random choice w/ examples

--- 3-synonymsAndAntonyms.py
+++ 3-synonymsAndAntonyms.py
@@ -1,5 +1,20 @@
 from nltk.corpus import wordnet
 
 
-syn = wordnet.synsets("test")
-print(syn)
+while True:
+    
+    request = input("Type a word: ")
+    if(request != 'q'):
+        syn = list()
+
+        ant = list()
+        for synset in wordnet.synsets(str(request)):
+            for lemma in synset.lemmas():
+                syn.append(lemma.name())    #add the synonyms
+                if lemma.antonyms():    #When antonyms are available, add them into the list
+                    ant.append(lemma.antonyms()[0].name())
+        print('Synonyms: ' + str(syn)+'\n')
+        print('Antonyms: ' + str(ant)+'\n')
+
+    else:
+        break;
